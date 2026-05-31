0c5e1f74261de62abbf8cfeee81cc1cd6a947e7f
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-05-31 10:35:47 +1000

    refactor: improve function declaration and add event listeners for DOM manipulation

--- Week_3/script.js
+++ Week_3/script.js
@@ -2,4 +2,19 @@
 let message = "Hello, JavaScript!";
 console.log(message);
 
-//function
+//function declaration
+function greetUser() {
+    alert("Welcome to JavaScript Fundamentals!");
+}
+
+//Calls function when page loads
+document.addEventListener("DOMContentLoaded", greetUser);
+
+//DOM Manipulation
+let button = document.getElementById("changeText");
+let text = document.getElementById("text");
+
+button.addEventListener("click", function() {
+    text.textContent = "You clicked the button!";
+    text.style.color = "green";
+})
