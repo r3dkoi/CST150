c9f76bf2c5a5380344676d4c56674c1d97840136
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-06-03 11:20:47 +1000

    Week 4 Duplication

---
+++ Week_4/script.js
@@ -0,0 +1,20 @@
+// Variables & Data Types
+let message = "Hello, JavaScript!";
+console.log(message);
+
+//function declaration
+function greetUser() {
+    alert("Welcome to JavaScript Fundamentals!");
+}
+
+//Calls function when page loads
+document.addEventListener("DOMContentLoaded", greetUser);
+
+//DOM Manipulation
+let button = document.getElementById("changeText");
+let text = document.getElementById("text");
+
+button.addEventListener("click", function() {
+    text.textContent = "You clicked the button!";
+    text.classList.toggle('text_colour_toggle');
+});
