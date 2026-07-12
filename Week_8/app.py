af704ce6ec5150084206eb65e56f62c0e993fb51
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-07-12 11:10:45 +1000

    moved opening sequence to end of file

--- Week_8/app.py
+++ Week_8/app.py
@@ -4,6 +4,31 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+@app.route('/about')
+def about():
+    return "This is the About Page"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
