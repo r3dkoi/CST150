f81d7cfb1d2489caf50edff74782f83f860734cb
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-07-14 11:51:40 +1000

    fix: correct SQL syntax and formatting for customer and menu item queries

--- Project_1/1_4 Exercise/queries.sql
+++ Project_1/1_4 Exercise/queries.sql
@@ -1,5 +1,13 @@
 /* -------------------------------------------------------------------------- */
 /*             Query 1: List all customers alphabetically by name             */
 /* -------------------------------------------------------------------------- */
-SELECT name from customers
+SELECT name FROM customers
 ORDER BY name ASC
+
+/* -------------------------------------------------------------------------- */
+/*           Query 2: List all menu items with prices less than $10           */
+/*                         ordered by Price ascending
+-------------------------------------------------------------------------- */
+SELECT name FROM menu_items
+WHERE price < 10.00
+ORDER by price ASC
