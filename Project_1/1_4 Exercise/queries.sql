a32d43889055bebdc72e8ed774519ddd666f7749
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-07-14 11:43:47 +1000

    feat: add SQL query to list customers alphabetically by name

---
+++ Project_1/1_4 Exercise/queries.sql
@@ -0,0 +1,5 @@
+/* -------------------------------------------------------------------------- */
+/*             Query 1: List all customers alphabetically by name             */
+/* -------------------------------------------------------------------------- */
+SELECT name from customers
+ORDER BY name ASC
